15526ffa6f5beb0e4c0d11bee07b2e1e8233204b
Yilin Fang <qzfyl98@outlook.com>
2026-01-17 23:57:20 -0500

    feat(pde): Remove zellij
    It does not support attaching back to the sessions created by previous versions

--- scripts/init/init.sh
+++ scripts/init/init.sh
@@ -49,10 +49,10 @@ if command -v lazygit &>/dev/null; then
 	alias lg='lazygit'
 fi
 
-# Use zj for zellij
-if command -v zellij &>/dev/null; then
-	alias zj='zellij'
-fi
+# # Use zj for zellij
+# if command -v zellij &>/dev/null; then
+# 	alias zj='zellij'
+# fi
 
 # Use y for yazi wrapper
 if command -v yazi &>/dev/null; then
