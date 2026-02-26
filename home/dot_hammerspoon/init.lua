f3f5a993a6fc53850f6a82be53f91a7b4c42d8a5
Yilin Fang <qzfyl98@outlook.com>
2026-02-26 10:11:24 -0500

    revert(hammerspoon): Revert "fix(hammerspoon): Cleanup"
    This reverts commit 3b911d785d67dc5f2a89d988525483bf475c8cd8.

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -9,8 +9,8 @@ local appShortcuts = {
 	{ "P", "Bitwarden" },
 	{ "W", "WeChat" },
 	-- { "C", "Visual Studio Code" },
-	-- { "C", "Cursor" },
-	{ "C", "Antigravity" },
+	{ "C", "Cursor" },
+	-- { "C", "Antigravity" },
 	-- { "F", "Firefox" },
 	-- { "F", "LibreWolf" },
 	{ "T", "TickTick" },
@@ -29,8 +29,8 @@ local linkShortcuts = {
 	-- { "N", "Notion", "https://www.notion.so/" },
 	-- { "M", "Monica", "https://monica.im/" },
 	-- { "D", "DeepSeek", "https://chat.deepseek.com/" },
-	-- { "G", "ChatGPT", "https://chatgpt.com/" },
-	{ "G", "Gemini", "https://gemini.google.com/" },
+	-- { "G", "Gemini", "https://gemini.google.com/" },
+	{ "G", "ChatGPT", "https://chatgpt.com/" },
 }
 
 -- Function to display notification and open an application
