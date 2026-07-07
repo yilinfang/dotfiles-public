5591e89f235f70f80ecae604d957a0bd31be7fe5
Yilin Fang <qzfyl98@outlook.com>
2026-07-07 01:42:02 -0400

    feat(misc): Update alias

--- scripts/init/init.sh
+++ scripts/init/init.sh
@@ -79,15 +79,15 @@ if command -v opencode &>/dev/null; then
 	alias oc='opencode'
 fi
 
-# Use c for codex
-if command -v codex &>/dev/null; then
-	alias c='codex'
-fi
-
-# # Use c for claude
-# if command -v claude &>/dev/null; then
-# 	alias c='claude'
+# # Use c for codex
+# if command -v codex &>/dev/null; then
+# 	alias c='codex'
 # fi
+
+# Use c for claude
+if command -v claude &>/dev/null; then
+	alias c='claude'
+fi
 
 # # Create wrappers for claude
 # if command -v claude &>/dev/null; then
