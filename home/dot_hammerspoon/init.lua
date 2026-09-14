8e2373f3fd9ae26d26b18584bae3aed1a804928d
Yilin Fang <qzfyl98@outlook.com>
2026-09-14 12:05:18 -0400

    feat(hammerspoon): keybindings

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -8,9 +8,9 @@ local appShortcuts = {
 	-- { "return", "kitty" },
 	{ "return", "Ghostty" },
 	{ "E", "Finder" },
-	{ "B", "Google Chrome" },
+	-- { "B", "Google Chrome" },
 	-- { "B", "Firefox" },
-	-- { "B", "Brave Browser" },
+	{ "B", "Brave Browser" },
 	-- { "B", "Vivaldi" },
 	-- { "P", "Bitwarden" },
 	{ "P", "1Password" },
@@ -25,12 +25,13 @@ local appShortcuts = {
 	{ "S", "Spotify" },
 	{ "O", "OpenInTerminal-Lite" },
 	-- { "N", "Obsidian" },
+	{ "N", "UpNote" },
 }
 
 -- General Shortcuts for Links
 local linkShortcuts = {
 	-- Key, Description, URL
-	{ "N", "Notion", "https://app.notion.com/" },
+	-- { "N", "Notion", "https://app.notion.com/" },
 	-- { "D", "DeepSeek", "https://chat.deepseek.com/" },
 	{ "G", "ChatGPT", "https://chatgpt.com/" },
 	-- { "G", "Gemini", "https://gemini.google.com/" },
